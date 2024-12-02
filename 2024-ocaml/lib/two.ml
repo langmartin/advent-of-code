d48b4e77d8737bc427d49a4b34bb97c664c7348e
Lang Martin <lang.martin@121.health>
2024-12-02 13:05:45 -0500

    can't figure out two stars

--- 2024-ocaml/lib/two.ml
+++ 2024-ocaml/lib/two.ml
@@ -9,25 +9,57 @@ let data file =
   |> One.read_lines
   |> List.map read_row
 
-let rec intersperse xs ys =
+(*
+ let rec intersperse xs ys =
   match (xs, ys) with
   | (x :: xs, y :: ys) -> x :: y :: intersperse xs ys
   | ([], _ys) -> []
-  | (x :: xs, []) -> x :: intersperse xs []
+   | (x :: xs, []) -> x :: intersperse xs []
+ *)
 
-let rec is_safe_lp op level =
+let rec is_safe_lp1 op level =
   match level with
-  | x :: y :: xs -> (op x y) && (abs (x - y) <= 3) && (is_safe_lp op xs)
+  | x :: y :: xs -> op x y && is_safe_lp1 op (y :: xs)
   | _ :: [] -> true
   | [] -> true
+
+let is_safe_pair op x y =
+  op x y && abs (x - y) <= 3
+
+let rec is_safe_lp tolerance op level =
+  match level with
+  | x :: y :: z :: xs ->
+    if op x y then
+      is_safe_lp tolerance op (y :: z :: xs)
+    else
+      tolerance > 0
+      && op x z
+      && is_safe_lp (tolerance - 1) op (z :: xs)
+  | x :: y :: [] ->
+    if tolerance > 0 then true else op x y
+  | _ :: [] -> true
+  | [] -> true
+
+let is_safe_but level =
+  match level with
+  | x :: y :: xs ->
+    let op = if x < y then ( < ) else ( > ) in
+    is_safe_lp1 (is_safe_pair op) level
+    (* is_safe_lp 0 (is_safe_pair op) level *)
+  | _ -> false
 
 let is_safe level =
   match level with
   | x :: y :: xs ->
     let op = if x < y then ( < ) else ( > ) in
-    intersperse level (y :: xs)
-    |> is_safe_lp op 
+    is_safe_lp 1 (is_safe_pair op) level
+    || is_safe_but (y :: xs)
   | _ -> false
+
+let unsafe_levels file =
+  file
+  |> data
+  |> List.filter (Fun.negate is_safe)
 
 let safe_levels file =
   file
@@ -35,6 +67,6 @@ let safe_levels file =
   |> List.filter is_safe
   |> List.length
 
-let part1_samp = safe_levels "../input/two-sample.txt"
+let samp = safe_levels "../input/two-sample.txt"
 
-let part1 = safe_levels "../input/two.txt"
+let real = safe_levels "../input/two.txt"
