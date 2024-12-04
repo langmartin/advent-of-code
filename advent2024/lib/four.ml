fbaef0c1680a01ea67a3b275d3915a0095bf2fb3
Lang Martin <lang.martin@121.health>
2024-12-04 15:12:44 -0500

    day four didn't try it yet

---
+++ advent2024/lib/four.ml
@@ -0,0 +1,83 @@
+let data =
+  "../input/four.txt"
+  |> One.read_lines
+  |> List.map (Fun.compose Array.of_seq String.to_seq)
+  |> Array.of_list
+
+let max_x data = Array.length data.(0) - 1
+let max_y data = Array.length data - 1
+
+let xmas = "XMAS"
+
+let next_c c =
+  let i = String.index xmas c in
+  if i < String.length xmas then
+    Some (String.get xmas (i + 1))
+  else
+    None
+
+let approach x xx =
+  if xx > x then
+    x + 1
+  else if xx < x then
+    x - 1
+  else
+    x
+
+let adv src trg =
+  let (x, y) = src in
+  let (xx, yy) = trg in
+  (approach x xx, approach y yy)
+
+let get x y data =
+  if x >= 0 &&
+     y >= 0 &&
+     x <= max_x data &&
+     y <= max_y data then
+    Some (data.(x).(y))
+  else
+    None
+
+let rec marks_the_spot src trg c data =
+  let (x, y) = src in
+  match get x y data with
+  | None -> false
+  | Some d -> d == c && begin
+      match next_c c with
+      | None -> true
+      | Some c -> marks_the_spot (adv src trg) trg c data
+    end
+
+let samp = [
+  ['X'; 'M'; 'A'; 'S']
+]
+
+let look_around src data =
+  let dist = String.length xmas in
+  let f = fun trg ->
+    if marks_the_spot src trg 'X' data then
+      Some (src, trg)
+    else
+      None
+  in
+  let (x, y) = src in
+  [(x + dist, y);
+   (x, y + dist);
+   (x + dist, y + dist);
+   (x - dist, y);
+   (x, y - dist);
+   (x - dist, y - dist)]
+  |> List.map f
+
+let search data =
+  let rec lp x y =
+    match (x == max_x data, y == max_y data) with
+    | (true, true) -> []
+    | (false, true) -> lp (x + 1) 0
+    | (_, false) ->
+      let c = data.(x).(y) in
+      if c == 'X' then
+        look_around (x, y) data :: lp x (y + 1)
+      else
+        lp x (y + 1)
+  
